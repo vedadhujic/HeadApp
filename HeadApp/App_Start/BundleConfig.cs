d06bc46e32be89dc9157968eaf77ec83eb27000b
Vedad Hujić <hujicvedad@outlook.com>
2020-08-25 00:55:32 +0200

    Minor additions.

--- HeadApp/App_Start/BundleConfig.cs
+++ HeadApp/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace HeadApp
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css",             
                       "~/Content/site.css"));
         }
     }
--- HeadApp/Models/Log.cs
+++ HeadApp/Models/Log.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HeadApp.Models
 {
@@ -12,6 +13,8 @@ namespace HeadApp.Models
 
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+        public IEnumerable<SelectListItem> Severity { get; set; }
 
         public string Description { get; set; }
     }
