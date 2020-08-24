1cd002203e1a98627f72b145a2ec4d6eea1ae53b
Vedad Hujić <hujicvedad@outlook.com>
2020-08-24 03:44:37 +0200

    First steps.

--- HeadApp/Models/Log.cs
+++ HeadApp/Models/Log.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace HeadApp.Models
     {
         public int logId { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
         public string Description { get; set; }
